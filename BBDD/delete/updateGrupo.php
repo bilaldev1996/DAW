e3d52907f7d0c3a885e0f6a062e9c78c4a977b4d
bilaldev1996 <bilalmessaoui96@gmail.com>
2022-11-08 12:22:51 +0100

    BBDD

--- BBDD/delete/updateGrupo.php
+++ BBDD/delete/updateGrupo.php
@@ -20,6 +20,7 @@
     $sql = "SELECT * FROM Grupo WHERE idGrupo = '$idGrupo'";
     $result = $conn->query($sql);
 
+    
 
     if($result->num_rows > 0){
         while($row = $result->fetch_assoc()){
