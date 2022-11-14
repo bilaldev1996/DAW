a6dc2796f9ca7a0703f03165307173b43d8ffbef
bilaldev1996 <bilalmessaoui96@gmail.com>
2022-11-14 17:08:46 +0100

    connect update

--- BBDD/connect.php
+++ BBDD/connect.php
@@ -1,15 +1,18 @@
 <?php
 
     /* 
-    Host: sql7.freesqldatabase.com
-    Database name: sql7548031
-    Database user: sql7548031
-    Database password: RE4yrrNWA2 */
+    To connect to your database use these details;
+
+    Host: sql8.freesqldatabase.com
+    Database name: sql8569824
+    Database user: sql8569824
+    Database password: UyEAE6yWUr
+    Port number: 3306 */
     
-    $serverName = "sql7.freesqldatabase.com";
-    $userName = "sql7548031";
-    $password = "RE4yrrNWA2";
-    $dbName = "sql7548031";
+    $serverName = "sql8.freesqldatabase.com";
+    $userName = "sql8569824";
+    $password = "UyEAE6yWUr";
+    $dbName = "sql8569824";
 
     // Create connection
     $conn = new mysqli($serverName, $userName, $password, $dbName);
